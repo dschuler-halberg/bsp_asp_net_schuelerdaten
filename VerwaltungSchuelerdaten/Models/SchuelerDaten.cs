b415eda64b31abe679482b9a7ae2df75bb9642e7
David Schuler <d.schuler@kbbz-halberg.de>
2021-01-25 17:25:28 +0100

    Änderungen Rechnerpraktikum 25. Januar

--- VerwaltungSchuelerdaten/Models/SchuelerDaten.cs
+++ VerwaltungSchuelerdaten/Models/SchuelerDaten.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,16 +17,23 @@ namespace VerwaltungSchuelerdaten.Models
 
     [DataType(DataType.Date)]
     public DateTime Geburtsdatum { get; set; }
+
+
+    [Display(Name = "E-Mail Adresse")]
     public string EMail { get; set; }
     public string Telefonnummer { get; set; }
     public string Konfession { get; set; }
+    
+    [Display(Name = "Staatsangehörigkeit")]
     public string Staatsangehoerigkeit { get; set; }
+   
+   
+    [DisplayName("Zweite Staatsangehörigkeit")]
     public string ZweiteStaatsangehoerigkeit { get; set; }
 
     [DataType(DataType.Date)]
     public DateTime Anmeldedatum { get; set; }
     public string Kommentar { get; set; }
-
 
     public static List<string> Konfessionen { get; set; } = 
       new List<string> { "katholisch", "evangelisch", "islam", "keine" };
